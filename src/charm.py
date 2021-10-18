8de5f91d2efd9a14f52725f9eda3de965d57a5f8
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2021-10-18 12:57:29 +0000

    Implement first version of registry charm

--- src/charm.py
+++ src/charm.py
@@ -4,100 +4,61 @@
 #
 # Learn more at: https://juju.is/docs/sdk
 
-"""Charm the service.
-
-Refer to the following post for a quick-start guide that will help you
-develop a new k8s charm using the Operator Framework:
-
-    https://discourse.charmhub.io/t/4208
-"""
-
 import logging
 
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, WaitingStatus, MaintenanceStatus
 
-logger = logging.getLogger(__name__)
+from charms.observability_libs.v0.kubernetes_service_patch import KubernetesServicePatch
+
+LOG = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 class RegistryCharm(CharmBase):
-    """Charm the service."""
+    """Charm for Docker registry service."""
 
     _stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
+        self.framework.observe(self.on.install, self._on_config_changed)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(things=[])
+        self.framework.observe(self.on.registry_pebble_ready, self._on_config_changed)
 
-    def _on_httpbin_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        You'll need to specify the right entrypoint and environment
-        configuration for your specific workload. Tip: you can see the
-        standard entrypoint of an existing container using docker inspect
-
-        Learn more about Pebble layers at https://github.com/canonical/pebble
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
+    def _on_config_changed(self, _):
+        container = self.unit.get_container("registry")
+        layer = {
+            "summary": "registry layer",
+            "description": "pebble config layer for registry",
             "services": {
-                "httpbin": {
+                "registry": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
+                    "summary": "registry",
+                    "command": "registry serve /etc/docker/registry/config.yml",
                     "startup": "enabled",
-                    "environment": {"thing": self.model.config["thing"]},
+                    "environment": {
+                        "REGISTRY_HTTP_PREFIX": self.config["prefix"],
+                    },
                 }
             },
         }
-        # Add intial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ActiveStatus()
+        if container.can_connect():
+            if container.get_plan().to_dict().get("services", {}) != layer["services"]:
+                self.unit.status = MaintenanceStatus("Unit is updating")
+                container.add_layer("registry", layer, combine=True)
+                LOG.info("Added container layer to Pebble")
+                LOG.debug("Restarting registry service")
+                container.restart("registry")
+                LOG.info("Restarted")
 
-    def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
-
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
-
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
-
-        Learn more about actions at https://juju.is/docs/sdk/actions
-        """
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
+            self.unit.status = ActiveStatus("Unit started")
         else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+            self.unit.status = WaitingStatus("Waiting for Pebble")
 
 
 if __name__ == "__main__":
