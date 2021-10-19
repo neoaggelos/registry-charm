e3356d40ad2c0f543dda53cc50268a140ee67cda
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2021-10-19 08:08:32 +0000

    Add nginx ingress integrator config

--- src/charm.py
+++ src/charm.py
@@ -12,6 +12,7 @@ from ops.main import main
 from ops.model import ActiveStatus, WaitingStatus, MaintenanceStatus
 
 from charms.observability_libs.v0.kubernetes_service_patch import KubernetesServicePatch
+from charms.nginx_ingress_integrator.v0.ingress import IngressRequires
 
 LOG = logging.getLogger(__name__)
 
@@ -21,6 +22,9 @@ class RegistryCharm(CharmBase):
 
     _stored = StoredState()
 
+    def service_hostname(self):
+        return self.config["external-hostname"] or self.app.name
+
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.install, self._on_config_changed)
@@ -28,8 +32,17 @@ class RegistryCharm(CharmBase):
         self.framework.observe(self.on.registry_pebble_ready, self._on_config_changed)
 
         self.service_patcher = KubernetesServicePatch(self, [("http", 5000)])
+        self.ingress = IngressRequires(
+            self,
+            {
+                "service-hostname": self.service_hostname(),
+                "service-name": self.app.name,
+                "service-port": 80,
+            },
+        )
 
     def _on_config_changed(self, _):
+        self.ingress.update_config({"service-hostname": self.service_hostname()})
         container = self.unit.get_container("registry")
         layer = {
             "summary": "registry layer",
@@ -43,7 +56,7 @@ class RegistryCharm(CharmBase):
                     "environment": {
                         "REGISTRY_HTTP_PREFIX": self.config["prefix"],
                     },
-                }
+                },
             },
         }
         if container.can_connect():
