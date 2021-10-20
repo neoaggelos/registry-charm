5529d8bd9be81e4913b9fae95f4a6285cd914cff
Aggelos Kolaitis <neoaggelos@gmail.com>
2021-10-20 16:13:10 +0300

    Add test using the registry charm

--- ops_tests/test_operator.py
+++ ops_tests/test_operator.py
@@ -1,3 +1,6 @@
+import json
+from urllib.request import urlopen
+
 import pytest
 from pytest_operator.plugin import OpsTest
 
@@ -7,3 +10,13 @@ async def test_build_and_deploy(ops_test: OpsTest):
     charm = await ops_test.build_charm(".")
     await ops_test.model.deploy(charm, resources={"registry-image": "registry:2"})
     await ops_test.model.wait_for_idle()
+
+
+async def test_using_charm(ops_test: OpsTest):
+    status = await ops_test.model.get_status()
+
+    address = status.applications["registry"].public_address
+    req = urlopen("http://{}:5000/v2/_catalog".format(address))
+
+    assert req.code == 200
+    assert json.loads(req.read()) == {"repositories": []}
