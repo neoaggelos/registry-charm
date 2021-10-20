7b3672ab9d3137fad61a4141ad5273904330fe2b
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2021-10-20 14:28:34 +0000

    Retry curl in registry

--- ops_tests/test_operator.py
+++ ops_tests/test_operator.py
@@ -4,6 +4,10 @@ from urllib.request import urlopen
 import pytest
 from pytest_operator.plugin import OpsTest
 
+import logging
+
+LOG = logging.getLogger(__name__)
+
 
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
@@ -16,7 +20,13 @@ async def test_using_charm(ops_test: OpsTest):
     status = await ops_test.model.get_status()
 
     address = status.applications["registry"].public_address
-    req = urlopen("http://{}:5000/v2/_catalog".format(address))
+    path = "http://{}:5000/v2/_catalog".format(address)
+    for _ in range(100):
+        try:
+            LOG.info("Trying %s", path)
+            req = urlopen(path, timeout=5)
+        except Exception as e:
+            LOG.warning("Exception %s, retry", e)
 
     assert req.code == 200
     assert json.loads(req.read()) == {"repositories": []}
